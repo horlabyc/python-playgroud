0855521fdd2083e95ca570f9c6754892bccea142
Olaosebikan Sulaiman <olabisisulaiman@gmail.com>
2023-03-21 13:16:21 +0100

    modify model

--- stock-tracker/model.py
+++ stock-tracker/model.py
@@ -1,15 +1,15 @@
 import datetime
 
 class StockItem:
-    def __init__(self, item_name, item_category, quantity, 
+    def __init__(self, name, category, quantity, 
                  created_at=None, updated_at=None, id=None
                 ):
-        self.item_name = item_name
-        self.item_category = item_category
+        self.name = name
+        self.category = category
         self.created_at = created_at if created_at is not None else datetime.datetime.now().isoformat()
         self.updated_at = updated_at if updated_at is not None else datetime.datetime.now().isoformat()
         self.quantity = quantity
         self.id = id if id is not None else None
     
     def __repr__(self) -> str:
-        return f"({self.item_name}, {self.item_category}, {self.quantity}, {self.created_at}, {self.updated_at}, {self.id})"
+        return f"({self.name}, {self.category}, {self.quantity}, {self.created_at}, {self.updated_at}, {self.id})"
